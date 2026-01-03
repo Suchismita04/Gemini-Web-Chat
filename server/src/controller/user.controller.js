aa5b0c072ac797d6957b3d9c5801a5d2ecc3b3c0
Suchismita04 <suchismitachatterjee13@gmail.com>
2026-01-03 21:35:13 +0530

    Define user registration route and controller

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -1,9 +1,78 @@
+import { User } from "../model/user.model.js";
+import bycrypt from 'bcrypt'
 
 
+
+//user registration
 const registerUser = async (req, res) => {
 
-    const { userName, email, password } = req.body()
+    const { username, email, password } = req.body
+
+    try {
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required"
+            })
+
+        }
+        const user = User
+
+        const isExistedUser = await user.findOne({ email })
+
+        if (isExistedUser) {
+            return res.status(409).json({
+                message: "User already exist"
+            })
+        }
+
+        const hashedPwd = await bycrypt.hash(password, 10)
+
+        const newUser = await user.create({
+            username,
+            email,
+            password: hashedPwd
+        })
+
+        return res.status(201).json({
+            message: "User has been created",
+            user: {
+                id: newUser._id,
+                username: newUser.username,
+                email: newUser.email
+
+            }
+
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message: "internal server error"
+        })
+    }
+}
+
+
+
+
+const logInUser= async (req,res)=>{
+    const {email,password}=req.body
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({
+                message:"All fields are required"
+            })
+        }
+     
+
+
+
+
+
+    } catch (error) {
+        
+    }
 
 }
 
-export { registerUser }
+export { registerUser,logInUser }
--- server/src/route/user.route.js
+++ server/src/route/user.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controller/user.controller.js";
+import { logInUser, registerUser } from "../controller/user.controller.js";
 
 const userRouter= Router()
 
 userRouter.post('/user/register',registerUser)
+userRouter.post('/user/logIn',logInUser)
 
 export { userRouter}
